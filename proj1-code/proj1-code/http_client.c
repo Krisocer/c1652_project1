8d11fb481ff4adf4893ca400499146225df4ef2c
Krisocer <lixiaozhimin@126.com>
2022-02-13 16:43:21 -0500

    add

--- proj1-code/proj1-code/http_client.c
+++ proj1-code/proj1-code/http_client.c
@@ -51,88 +51,15 @@ main(int argc, char ** argv)
         exit(-1);
     }
 
-//    /*
-//     * NULL accesses to avoid compiler warnings about unused variables
-//     * You should delete the following lines
-//     */
-//    (void)server_name;
-//    (void)server_port;
+    /*
+     * NULL accesses to avoid compiler warnings about unused variables
+     * You should delete the following lines 
+     */
+    (void)server_name;
+    (void)server_port;
 
     /* make socket */
-    int sock;
-    if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
-        fprintf(stderr, "socket error");
-        exit(-1);
-    }
-    struct hostent *hp;
-    if((hp = gethostbyname(server_name)) == NULL){
-        fprintf(stderr, "server name error");
-        exit(-1);
-    }
 
-    //give address
-    struct sockaddr_in saddr;
-    memset(&saddr, 0, sizeof(saddr));
-    saddr.sin_family = AF_INET;
-    memcpy(&saddr.sin_addr.s_addr, hp->h_addr, hp->h_length);
-    saddr.sin_port = htons(server_port);
-    printf("Trying %s...\n", inet_ntoa(saddr.sin_addr));
-
-    if(connect(sock, (struct sockaddr *)&saddr, sizeof(saddr)) < 0){
-        perror("could not connect to server");
-        exit(-1);
-    }
-    printf("connected to %s\n", server_name);
-
-    req_str = malloc(100);
-    sprintf(req_str, "GET /%s HTTP/1.0\r\n\r\n",server_path);
-
-    int res;
-    int len = strlen(req_str);
-    if((res = send(sock, req_str, len, 0)) <= 0){
-        perror("send error");
-        exit(-1);
-    }
-    printf("Request made: %s\n",req_str);
-
-    fd_set fds;
-    FD_ZERO(&fds);
-    FD_SET(sock, &fds);
-    select(sock+1, &fds, NULL, NULL, NULL);
-
-    char buf[BUFSIZE];
-    char header[BUFSIZE];
-    char bufcpy[BUFSIZE];
-    int head_end = 0;
-    while(res > 0 && head_end == 0){
-        res = recv(sock, buf, BUFSIZE-1, 0);
-        for(int i = 0; i < BUFSIZE; i++){
-            if(buf[i] == '\r' && buf[i+1] == '\n' && buf[i+2] == '\r' && buf[i+3] == '\n'){
-                head_end = 1;
-                strncpy(header,&buf[0],i);
-                strcpy(bufcpy,&buf[i]);
-            }
-        }
-        buf[res] = '\0';
-    }
-    char OK_check[3];
-    strncpy(OK_check,&header[9],3);
-    if(atoi(OK_check) == 200){
-        printf("%s\n",header);
-        printf("%s", bufcpy);
-        while(res > 0){
-            res = recv(sock, buf, BUFSIZE-1, 0);
-            buf[res] = '\0';
-            printf("%s",buf);
-        }
-        close(sock);
-        return 0;
-    }else{
-        //printf("test");
-        printf("%s\n",header);
-        close(sock);
-        return -1;
-    }
     /* get host IP address  */
     /* Hint: use gethostbyname() */
 
